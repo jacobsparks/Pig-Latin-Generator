33010c290740666a4a7dffff43f005d182da1dda
jacobsparks <38540611+jacobsparks@users.noreply.github.com>
2018-12-27 16:16:54 -0500

    Add files via upload

---
+++ PigLatinTranslator.py
@@ -0,0 +1,21 @@
+# this program translates any word you input into pig latin
+
+
+userWord = input("Enter any word:")
+
+pig = "ay"
+
+
+if len(userWord) > 0 and userWord.isalpha():
+    word = userWord.lower()
+    first = word[0]
+    new = word[1:] + first + pig
+    print(new)
+else:
+    print("INPUT INVALID")
+
+input("hi")
+
+
+
+
